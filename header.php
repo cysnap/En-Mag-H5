d8ca9543d2b5139bc126a6afcec23841b6d0f438
Yong Chen <cysnap@gmail.com>
2017-03-18 16:52:00 +0800

    everything is okay

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en_US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+	<title><?php if(is_single() || is_page() || is_archive() || is_404() || is_search()) : wp_title('_',true,'right'); endif; bloginfo('name');  if( $paged == "" ) $pagenum = "";else echo $pagenum = " - 第 ".$paged." 页"; ?></title>
+	<?php if(strpos($_SERVER['HTTP_USER_AGENT'],'AppleWebKit') !== false) :?>
+	<link rel="apple-touch-icon" href="/iOS_icon.png"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<?php endif;?>
+	<?php if(is_home() && !is_paged()) :?>
+		<meta name="description" content="<?php echo bloginfo('description'); ?>" />
+		<?php endif; if(is_single()) :?>
+		<meta name="description" content="<?php echo mb_substr(strip_tags($post->post_content),0,120); ?>" />
+		<?php endif; if(is_category() && !is_paged()) :?>
+		<meta name="description" content="<?php echo strip_tags(category_description()); ?>" />
+	<?php endif;?>
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url');_e('?');echo(date('Ynj', time()));?>" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/functions.js"></script>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+<header>
+	<div class="nova-l-h">
+			<div class="nova-l"><a href="<?php echo get_settings('home'); ?>" title="<?php bloginfo('name'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.gif" class="logo" alt="<?php bloginfo('name'); ?>" /></a></div>
+			<div class="flat_image">
+				<ul>
+					<?php wp_reset_query(); ?>
+					<?php query_posts($query_string . 'meta_key=flat_image&posts_per_page=3'); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<li>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img alt="<?php the_title(); ?>" src="<?php $key="flat_image"; echo get_post_meta($post->ID, $key, true); ?>&w=182&h=100&crop-to-fit"/>
+						<span><?php the_title(); ?></span></a>
+					</li>
+					<?php endwhile; endif; ?>
+					<?php wp_reset_query(); ?>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+	</div>
+	<div class="nav">
+		<nav>
+			<?php $h5menu = wp_nav_menu(array('theme_location'=>'header-menu','container'=>false,'items_wrap'=>'%3$s','echo'=>false,)); $find=array('><a','</a>','li'); $replace=array('','','a'); echo str_replace($find,$replace,$h5menu);?>
+			<div class="search">
+			<?php if (!is_search()) {$search_text = "Search";} else {$search_text = "$s";} ?>
+				<form method="get" class="searchform" name="searchform" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+					<input type="text" class="search-input nova-l" size="24" value="Search" name="s" onfocus="if (this.value == 'Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search';}" />
+				</form>
+			</div>
+		</nav>
+	</div>
+</header>
+
+<main>
+	<div class="content">
---
+++ index.php
@@ -0,0 +1,96 @@
+<?php get_header(); ?>
+
+<?php if ( is_home() && !is_paged() ) : ?>
+<div class="headflash">
+	<script>$(function() {$("#rslides").responsiveSlides({auto: true,pager: false,speed: 500,maxwidth: 260})});</script>
+	<div class="flashNews" id="rslides">
+			<?php wp_reset_query(); ?>
+			<?php query_posts($query_string . 'meta_key=flash_image&posts_per_page=4'); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<figure>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_blank">
+					<img alt="<?php the_title(); ?>" src="<?php $key="flash_image"; echo get_post_meta($post->ID, $key, true); ?>"/>
+					<span><?php the_title(); ?></span>
+				</a>
+			</figure>
+			<?php endwhile; endif; ?>
+			<?php wp_reset_query(); ?>
+	</div>
+
+	<div class="headrcmd">
+			<?php query_posts($query_string . 'tag=recommended&posts_per_page=6'); ?>
+			<?php $count = 1;if ( have_posts() ) : while ( have_posts() ) : the_post(); if ($count == 1) : ?>
+			<div class="headsgst">
+				<dl>
+				<dt><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></dt>
+				<dd><?php echo mb_strimwidth(strip_tags(apply_filters('the_excerpt', $post->post_content)), 0, 120,"..."); ?> <small>[<a href="<?php the_permalink(); ?>" >查看详情</a> - <?php the_views($display = false); ?>&nbsp;次浏览]</small></dd>
+				</dl>
+			</div>
+			<div class="headsgst">
+				<ul>
+					<?php else: ?>
+					<li><span class="sidebar-count">(<?php the_views($display = false); ?>)</span><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo mb_strimwidth(get_the_title(), 0, 40, '...'); ?></a></li>
+					<?php endif; $count++; endwhile; endif; ?>
+					<?php wp_reset_query(); ?>
+				</ul>
+			</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<?php endif; ?>
+
+<div class="post-block">
+	<?php wp_reset_query(); ?>
+	<?php $rcmdcounter = 0; ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php $rcmdcounter++; ?>
+	<div class="post">
+		<div class="post-title">
+			<?php if(is_sticky()) : ?>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>" target="_blank"><?php the_title(); ?></a><?php _e('<span style="color:red;">[置顶] </span>'); ?></h2>
+			<?php else: ?>
+			<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>" target="_blank"><?php the_title(); ?></a><?php video_tag(); ?><?php $diff = strtotime(date('Y-m-d H:i:s'))-strtotime(get_the_time('Y-m-d H:i:s')); if ($diff <= -25100){ ?><img alt="新" src="<?php echo get_stylesheet_directory_uri(); ?>/images/new-icon.gif"><?php } ?></h2>
+			<?php endif; ?>
+			<span class="post-count"><?php echo($rcmdcounter); ?>#</span>
+		</div>
+
+		<div class="entry clearfix">
+			<div class="nova-r">
+				<?php
+					$key = "cover_s";
+					$cover_s_meta = get_post_meta($post->ID,$key,TRUE);
+					if ($cover_s_meta != ""):
+				?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img alt="<?php the_title(); ?>" src="<?php echo $cover_s_meta;?>&w=120&h=92&crop-to-fit" style="max-width:120px;padding:5px;"></a>
+				<?php else: ?>
+				<?php
+				$the_cat = get_the_category();
+				$category_slug = $the_cat[0]->slug;
+				$category_name = $the_cat[0]->cat_name;
+				$category_description = $the_cat[0]->category_description;
+				$category_link = get_category_link( $the_cat[0]->cat_ID );
+				?>
+				<a href="<?php echo $category_link; ?>" title="<?php echo $category_name; ?>"><img alt="<?php echo $category_name; ?>" src="<?php img_url();?>/cat-img/<?php echo $category_slug; ?>.gif" /></a>
+				<?php endif; ?>
+			</div>
+		<?php the_content('',TRUE,'');?>
+
+		</div>
+
+		<div class="post-bottom clearfix">
+			<span class="post-info"><?php the_author(); ?>&nbsp;Posted in &nbsp;: <time><?php the_time('m-d-Y H:i'); ?></time>&nbsp;-&nbsp; Topics : <?php foreach((get_the_category()) as $category) {echo $category->cat_name . ' ';}?>&nbsp;-&nbsp;<?php the_views($display = false); ?>&nbsp;views</span>
+			<span class="nova-r">
+				<?php comments_popup_link('DISCUSSION&nbsp;&raquo;', '1 comment&nbsp;&raquo;', '% comments&nbsp;&raquo;', 'post-comments'); ?>&nbsp;&nbsp;&nbsp;<a class="more-link" href="<?php the_permalink() ?>" title="<?php the_title(); ?>" target="_blank" rel="nofollow">Read More&nbsp;&raquo;</a>
+			</span>
+		</div>
+	</div>
+
+	<?php endwhile; ?>
+	</div>
+	<?php pagenavi(); else : ?>
+	<div class="fuss nova">Nothing Here!</div>
+	<?php endif; ?>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
